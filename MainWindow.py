e056a58c185ec22c238fb47a07a70f8bfbd8f552
Ben Kozlowski <kozlowski.ben@gmail.com>
2024-02-01 14:16:58 -0800

    Added comments

--- MainWindow.py
+++ MainWindow.py
@@ -1,59 +1,83 @@
+#Import global classes
 import requests, json
 from prettytable import PrettyTable
 
+#Import PySide classes
 from PySide6.QtWidgets import QMainWindow, QMessageBox, QHeaderView
 from PySide6.QtGui import QStandardItemModel, QStandardItem
 
+#Import local classes
 from ui.ui_mainwindow import Ui_MainWindow
 from LocalDatabase import LocalDatabase
 
+#Set path signal threshold. Add this to settings later.
 PATH_SS_THRESHOLD = 3
 
 class MainWindow(QMainWindow):
 
     def __init__(self, parent=None):
+        #Setup Qt UI
         super().__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+
+        #Add click actions to top menu
         self.ui.actionExit.triggered.connect(self.close)
         self.ui.actionSync.triggered.connect(self.downloadData)
         self.ui.actionAbout.triggered.connect(self.showAbout)
 
+        #When callsign text box text is changed
         self.ui.callsignEdit.textChanged.connect(self.loadData)
+
+        #When TX/RX selector is changed
         self.ui.txRxSelect.currentIndexChanged.connect(self.loadData)
 
+        #Set table resize mode. This makes the table use the entire width of its parent
         self.ui.signalTableView.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
+        #Load db into variable
         self.ldb = LocalDatabase()
 
+        #Search local database and display data. This starts as anb empty callsign, returning all entries.
         self.loadData()
+
+        #Generate relay path tables
         self.generateRelayPaths()
 
     def downloadData(self):
+        #Download all current data from the server and store it locally into database
         try:
-            x = requests.get("https://erc.iegmrs.com/data/callsign/dump")
-            self.ldb.syncCallsignData(json.loads(x.text))
+            x = requests.get("https://erc.iegmrs.com/data/callsign/dump")   #Fetch all current data into json string
+            self.ldb.syncCallsignData(json.loads(x.text))                   #Load string into array and push to database
 
-            x = requests.get("https://erc.iegmrs.com/data/signal/dump")
-            self.ldb.syncSignalData(json.loads(x.text))
+            x = requests.get("https://erc.iegmrs.com/data/signal/dump")     #Fetch all current data into json string
+            self.ldb.syncSignalData(json.loads(x.text))                     #Load string into array and push to database
 
         except requests.ConnectionError:
-            pass #Offline
+            #Fails when there is no internet connection.
+            pass
 
         self.loadData()
         self.generateRelayPaths()
 
     def loadData(self):
+        #Get callsign text
         callsign = self.ui.callsignEdit.text()
+
+        #Get RX/TX dropdown and compare if it is selected 'RX' or 'TX'
         if self.ui.txRxSelect.currentText() == 'RX':
+            #Fetch all callsigns that {callsign} heard 
             cs_data_list = self.ldb.fetchRxSignal(callsign)
         else:
+            #Fetch all callsigns that heard {callsign}
             cs_data_list = self.ldb.fetchTxSignal(callsign)
 
+        #Create table model and set table headers
         model = QStandardItemModel()
         model.setHorizontalHeaderLabels(['RX', 'TX', 'SS', 'DATE', 'LAT/LNG'])
 
+        #Loop through returned entries and insert into table
         for e in cs_data_list:
             row = []
             row.append(QStandardItem(e.rx))
@@ -63,16 +87,22 @@ class MainWindow(QMainWindow):
             row.append(QStandardItem(e.lat + ', ' + e.lng))
             model.appendRow(row)
 
+        #Set model to table to display the results.
         self.ui.signalTableView.setModel(model)
 
     def generateRelayPaths(self):
         cs_data_list = self.ldb.fetchCallsigns()
 
+        #Get all callsigns that are currently in the Signals table. These are the contactable stations. Excludes stations with a signal below the SS Threashold
         rx_cs_list = self.getContactableCallsignList(cs_data_list)
+
+        #Based on the contactable callsign list, create a score based on the signal score summed from all stations that RX that specific station.
         score_list = self.createScoreList(cs_data_list, rx_cs_list)
 
+        #Output SS threshold
         output = f"PATH SS THRESHOLD: {PATH_SS_THRESHOLD}\n\n"
 
+        #Output Relay Signal Score table
         output += "RELAY SIGNAL SCORE\n"
         table = PrettyTable()
         table.field_names = ['Callsign', 'Score', 'RX Count']
@@ -80,16 +110,18 @@ class MainWindow(QMainWindow):
             table.add_row(r[:3])
         output += str(table)
 
+        #Output Primary Relay Path table
         output += "\n\nPRIMARY RELAY PATH\n"
         relay_list, non_contact_list = self.createRelayList(cs_data_list, 0)
         output += self.createRelayTable(relay_list)
 
+        #Output Alternate Relay Path table
         output += "\n\nALTERNATE RELAY PATH\n"
         relay_list, non_contact_list = self.createRelayList(cs_data_list, 1)
         output += self.createRelayTable(relay_list)
-
         output += '\n\n'
 
+        #Output Isolated Callsigns table. These are callsigns that have entries for TX but no log for RX signals
         table = PrettyTable()
         table.field_names = ['Isolated Callsigns']
         for r in sorted(non_contact_list):
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import os, sys
 
+#Import PySide classes
 from PySide6.QtCore import QSize
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
+
+#Import local classes
 from Constants import SETTINGS_DIR, RESOURCE_PATH
 from LocalDatabase import LocalDatabase
 from MainWindow import MainWindow
 
+#If main program run this.
 if __name__ == "__main__":
 
     #Setup settings directory if it doesn't exist
@@ -18,8 +22,10 @@ if __name__ == "__main__":
     ldb.validate()
     ldb.setup()
 
+    #Start Qt Application
     app = QApplication(sys.argv)
 
+    #Load icon as windows icon
     app_icon = QIcon()
     app_icon.addFile(f'{RESOURCE_PATH}/iegmrs.png', QSize(16,16))
     app_icon.addFile(f'{RESOURCE_PATH}/iegmrs.png', QSize(24,24))
@@ -28,7 +34,9 @@ if __name__ == "__main__":
     app_icon.addFile(f'{RESOURCE_PATH}/iegmrs.png', QSize(256,256))
     app.setWindowIcon(app_icon)
 
+    #Show main window
     window = MainWindow()
     window.show()
 
+    #Exit when window is closed
     app.exec()
